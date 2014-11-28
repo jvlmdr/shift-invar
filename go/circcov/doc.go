abd42f086c1d3b4d6d0c9cef25f178d99c7036d6
Jack Valmadre <jack.valmadre@gmail.com>
2014-11-28 22:11:32 +0000

    circcov: Add short package doc

---
+++ go/circcov/doc.go
@@ -0,0 +1,7 @@
+/*
+Package circcov addresses circulant covariance matrices.
+
+Circulant covariance matrices are derived from Toeplitz ones.
+There are a number of ways in which to do this, specified by CoeffsFunc.
+*/
+package circcov
